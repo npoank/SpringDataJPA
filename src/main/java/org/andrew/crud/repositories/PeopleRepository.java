1ecb1367d584730aa11d2ae0f51e37adf86cc540
Kulbicki Andrey <kubl-ml@mail.ru>
2022-12-26 15:44:07 +0300

    implementation Spring Data JPA

---
+++ src/main/java/org/andrew/crud/repositories/PeopleRepository.java
@@ -0,0 +1,9 @@
+package org.andrew.crud.repositories;
+
+import org.andrew.crud.models.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+}
