06483626a67243b795231d291a1cb4f09316830b
Kulbicki Andrey <kubl-ml@mail.ru>
2022-12-26 14:59:11 +0300

    clean

---
+++ src/main/java/org/andrew/crud/models/Person.java
@@ -0,0 +1,76 @@
+package org.andrew.crud.models;
+
+
+
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Name must not be empty")
+    @Size(min = 2, max = 30, message = "Invalid name size (2-30 symbols)")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 0, message = "Minimal age 0")
+    @Column(name = "age")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Invalid email")
+    @Column(name = "email")
+    private String email;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+}
